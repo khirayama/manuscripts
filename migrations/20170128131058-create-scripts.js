8cf172959dd0a30e3a2600351222886928234c39
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-28 22:20:29 +0900

    Create scripts table migration file

---
+++ migrations/20170128131058-create-scripts.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('scripts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('scripts');
+  }
+};
