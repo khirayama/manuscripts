5c98cbcc97b53376bff66a31829e4c2eb10faa91
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-29 10:49:53 +0900

    Replace url to link

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {Script, Link} from './models';
 
+const patterns = {
+  //URLs starting wth http://, https://, ftp:// or /
+  LINK: /((https?:\/|ftp:\/|)\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim,
+
+  //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+  WWW_LINK: /(^|[^\/])(www\.[\S]+(\b|$))/gim,
+};
+
+function linkify(inputText) {
+  let replacedText = '';
+  replacedText = inputText.replace(patterns.LINK, '<a href="$1" target="_blank">$1</a>');
+  replacedText = replacedText.replace(patterns.WWW_LINK, '$1<a href="http://$2" target="_blank">$2</a>');
+  return replacedText;
+}
+
 const app = express();
 
 app.set('views', 'src/views');
@@ -31,8 +46,10 @@ app.get('/scripts/new', (req, res) => {
 app.get('/scripts/:id', (req, res) => {
   const scriptId = req.params.id;
   Script.findById(scriptId).then(script => {
+    const scriptBodyHTML = linkify(script.body);
     res.render('scripts/show', {
       script,
+      scriptBodyHTML,
     });
   });
 });
