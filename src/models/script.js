703f1f1c5eb3974c0dbb77a9e88ee2fbf50186eb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-29 09:01:01 +0900

    Create script model

---
+++ src/models/script.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Script', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at',
+    },
+  });
+};
