f06eaa4dff5ca475fbd6a0221211c34f2fae5ac1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-29 09:36:07 +0900

    Introduce pug as view template

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.set('views', 'src/views')
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('dashboard/index', {
+    title: 'Hey',
+    message: 'Hello there!',
+  });
 });
 
 app.listen(3000, () => {
