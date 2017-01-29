f8d635fa63e25e1e04aa8b9f6b304d50cff3f9e7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-01-29 09:26:17 +0900

    Create link model

---
+++ src/models/link.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Link', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at',
+    },
+  });
+};
